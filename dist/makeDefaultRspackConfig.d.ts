31f80ea6c6640f8d7d6d10372da484cdec5b5ea7
joseph.feng <joseph.feng@cartrack.com>
2023-11-11 20:27:14 +0800

    chore: rename the makeDefaultWebpackConfig to makeDefaultRspackConfig

--- dist/makeDefaultRspackConfig.d.ts
+++ dist/makeDefaultRspackConfig.d.ts
@@ -1,3 +1,3 @@
 import type { Configuration } from '@rspack/core';
 import type { CreateFinalRspackConfig } from './createRspackDevServer';
-export declare function makeCypressWebpackConfig(config: CreateFinalRspackConfig): Configuration;
+export declare function makeCypressRspackConfig(config: CreateFinalRspackConfig): Configuration;
