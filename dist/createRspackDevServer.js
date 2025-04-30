a61a62645dc568e2988baf66bc773c54946106a5
Feng Xue <xfsnowind@gmail.com>
2025-04-30 17:30:07 +0800

    chore: upgrade rspack to 1.3.8 (#39)
    * chore: upgrade rspack to 1.3.8
    
    * chore: update pnpm to version 10.10.0

--- dist/createRspackDevServer.js
+++ dist/createRspackDevServer.js
@@ -1,10 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.createRspackDevServer = createRspackDevServer;
-const tslib_1 = require("tslib");
-const debug_1 = tslib_1.__importDefault(require("debug"));
 const makeRspackConfig_1 = require("./makeRspackConfig");
-const debug = (0, debug_1.default)('cypress-rspack-dev-server:start');
 async function createRspackDevServer(config) {
     var _a;
     const { sourceRspackModulesResult: { rspack: { module: rspack }, rspackDevServer: { module: RspackDevServer }, }, devServerConfig: { cypressConfig: { devServerPublicPathRoute, isTextTerminal }, }, } = config;
@@ -13,7 +10,7 @@ async function createRspackDevServer(config) {
     const isOpenMode = !isTextTerminal;
     const rspackDevServerConfig = Object.assign(Object.assign({ host: '127.0.0.1', port: 'auto' }, finalRspackConfig.devServer), { devMiddleware: {
             publicPath: devServerPublicPathRoute,
-            stats: ((_a = finalRspackConfig.stats) !== null && _a !== void 0 ? _a : 'minimal'),
+            stats: (_a = finalRspackConfig.stats) !== null && _a !== void 0 ? _a : 'minimal',
         }, hot: false, 
         // Only enable file watching & reload when executing tests in `open` mode
         liveReload: isOpenMode, client: { overlay: false } });
--- src/createRspackDevServer.ts
+++ src/createRspackDevServer.ts
@@ -1,11 +1,8 @@
 import debugLib from 'debug'
 import type { Configuration } from '@rspack/dev-server'
-import type { StatsOptions } from 'webpack'
 import type { DevServerConfig } from './devServer'
 import type { SourceRelativeRspackResult } from './helpers/sourceRelativeRspackModules'
 import { makeRspackConfig } from './makeRspackConfig'
-
-const debug = debugLib('cypress-rspack-dev-server:start')
 
 /**
  * Takes the rspack / rspackDevServer modules, the configuration provide
@@ -49,7 +46,7 @@ export async function createRspackDevServer(config: CreateFinalRspackConfig) {
     ...finalRspackConfig.devServer,
     devMiddleware: {
       publicPath: devServerPublicPathRoute,
-      stats: (finalRspackConfig.stats ?? 'minimal') as StatsOptions,
+      stats: finalRspackConfig.stats ?? 'minimal',
     },
     hot: false,
     // Only enable file watching & reload when executing tests in `open` mode
